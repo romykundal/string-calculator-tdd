43958ac9bd05f3d75a9519bc1f4d001047d60a47
romykundal <romykundal@hotmail.co.uk>
2024-05-21 11:03:22 +0530

    handle an any amount of numbers cases are added

--- test/test.js
+++ test/test.js
@@ -8,5 +8,17 @@ describe('StringCalculator', () => {
         expect(StringCalculator.add("")).to.equal(0);
     });
 
+    it('should return the number itself for a single number', () => {
+        expect(StringCalculator.add("1")).to.equal(1);
+    });
+
+    it('should return the sum of two numbers', () => {
+        expect(StringCalculator.add("1,2")).to.equal(3);
+    });
+
+    it('should return the sum of multiple numbers', () => {
+        expect(StringCalculator.add("1,2,3,4")).to.equal(10);
+    });
+
 
 });
